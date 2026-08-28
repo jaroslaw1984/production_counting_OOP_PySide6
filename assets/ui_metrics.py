782fc2dcc17a5f95b97cc295d9c4ca04722da2c3
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2026-08-28 14:52:25 +0200

    ui(config) - create configuration values for the program

---
+++ assets/ui_metrics.py
@@ -0,0 +1,25 @@
+from PySide6.QtCore import QSize
+
+
+# -----------------------
+# /// main_windows.py \\\
+# -----------------------
+
+# Main window configuration
+MAIN_WINDOW_SIZE = QSize(840,640)
+MAIN_LAYOUT_MARGINS = (0, 0, 0, 0)
+MAIN_LAYOUT_SPACING = 0
+
+# Left panel configuration
+LEFT_PANEL_WIDTH = 220
+LEFT_PANEL_MARGINS = (15, 20, 15, 20)
+LEFT_PANEL_SPACING = 15
+
+# Right panel configuration
+RIGHT_PANEL_MARGINS = (20, 20, 20, 20)
+
+# Welcome card configuration
+WELCOME_CARD_SIZE  = QSize(550, 350)
+WELCOME_SPACING = 10
+WELCOME_LOGO_BOTTOM_SPACING = 10
+WELCOME_VERSION_TOP_SPACING = 20
